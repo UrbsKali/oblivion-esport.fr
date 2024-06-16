68787eba87c9b23a51eb951cb66b0c3303b79fbd
Urbain <urbain.lantres@tutanota.com>
2024-06-16 19:18:37 +0200

    New inscription page + backend + DB

--- inscription.php
+++ inscription.php
@@ -1,4 +1,6 @@
 <?php
+// get the database connection
+require_once('db.php');
 
 $forms = getFormsData();
 $parsed_forms = parseForms($forms);
@@ -6,6 +8,7 @@ if (isset($parsed_forms["error"])) {
     header("Location: /page/inscription.html?message=" . $parsed_forms["error"]);
     exit(0);
 }
+saveToDB($parsed_forms);
 
 $msg = "Bonjour " . $parsed_forms['nom_equipe'] . ",<br><br> Nous avons bien reçu votre inscription pour notre event. <br><br> Merci de votre participation, et à bientôt dans la faille !";
 $to = $parsed_forms["top_email"] . "," . $parsed_forms["jungle_email"] . "," . $parsed_forms["middle_email"] . "," . $parsed_forms["bot_email"] . "," . $parsed_forms["support_email"];
@@ -14,42 +17,40 @@ $error_one = sendMail($to, "Confirmation d'inscription", $msg);
 
 
 
-$msg_admin = "Une nouvelle équipe vient de s'inscrire à l'event : " . $parsed_forms['nom_equipe'] . "<br><br> Voici les informations de l'équipe : <br><br>";
-// add all form data to the admin message
-$msg_admin = "<table border='1'>";
-$msg_admin .= "<tr><th>Champs</th><th>Valeur</th></tr>";
-$msg_admin .= "<tr><td>Nom d'équipe</td><td>" . $parsed_forms["nom_equipe"] . "</td></tr>";
-$msg_admin .= "<tr><td>TAG d'équipe</td><td>" . $parsed_forms["tag_equipe"] . "</td></tr>";
-$msg_admin .= "<tr><td>top_name</td><td>" . $parsed_forms["top_name"] . "</td></tr>";
-$msg_admin .= "<tr><td>top_discord</td><td>" . $parsed_forms["top_discord"] . "</td></tr>";
-$msg_admin .= "<tr><td>top_email</td><td>" . $parsed_forms["top_email"] . "</td></tr>";
-$msg_admin .= "<tr><td>jungle_name</td><td>" . $parsed_forms["jungle_name"] . "</td></tr>";
-$msg_admin .= "<tr><td>jungle_discord</td><td>" . $parsed_forms["jungle_discord"] . "</td></tr>";
-$msg_admin .= "<tr><td>jungle_email</td><td>" . $parsed_forms["jungle_email"] . "</td></tr>";
-$msg_admin .= "<tr><td>middle_name</td><td>" . $parsed_forms["middle_name"] . "</td></tr>";
-$msg_admin .= "<tr><td>middle_discord</td><td>" . $parsed_forms["middle_discord"] . "</td></tr>";
-$msg_admin .= "<tr><td>middle_email</td><td>" . $parsed_forms["middle_email"] . "</td></tr>";
-$msg_admin .= "<tr><td>bot_name</td><td>" . $parsed_forms["bot_name"] . "</td></tr>";
-$msg_admin .= "<tr><td>bot_discord</td><td>" . $parsed_forms["bot_discord"] . "</td></tr>";
-$msg_admin .= "<tr><td>bot_email</td><td>" . $parsed_forms["bot_email"] . "</td></tr>";
-$msg_admin .= "<tr><td>support_name</td><td>" . $parsed_forms["support_name"] . "</td></tr>";
-$msg_admin .= "<tr><td>support_discord</td><td>" . $parsed_forms["support_discord"] . "</td></tr>";
-$msg_admin .= "<tr><td>support_email</td><td>" . $parsed_forms["support_email"] . "</td></tr>";
-$msg_admin .= "<tr><td>have_logo</td><td>" . ($parsed_forms["have_logo"] == "on" ? "Oui" : "Non") . "</td></tr>";
-$msg_admin .= "<tr><td>is_new</td><td>" . ($parsed_forms["is_new"] == "on" ? "Oui" : "Non") . "</td></tr>";
-$msg_admin .= "<tr><td>nb_old_player</td><td>" . $parsed_forms["@nb_old_player"] . "</td></tr>";
-$msg_admin .= "</table>";
+$msg_admin = "Une nouvelle équipe vient de s'inscrire à l'event : " . $parsed_forms['nom_equipe'] . "<br><br> Vous trouverez les informations sur le dashboard ADMIN<br><br>";
 
-$error_two = sendMail("urbain.lantres@gmail.com", "Nouvelle inscription", $msg_admin);
+$error_two = sendMail("thomas.jego@protonmail.com,kyliann94@gmail.com", "Nouvelle inscription", $msg_admin);
 
 header("Location: /page/inscription.html?message=Votre inscription a bien été prise en compte");
 exit(0);
+
 /**
  * Récupère les données du formulaire
  * @return array
  */
 function getFormsData()
 {
+    // Get file upload
+    if ($_FILES['logo']['size'] == 0) {
+        $_POST['have_logo'] = 0;
+    } else {
+        $_POST['have_logo'] = 1;
+    } 
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["logo"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["logo"]["tmp_name"]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        } else {
+            $uploadOk = 0;
+        }
+    }
+    if ($uploadOk == 1) {
+        move_uploaded_file($_FILES["logo"]["tmp_name"], $target_file);
+    }
     // Capture all form data into an associative array
     $forms = array(
         'nom_equipe' => $_POST['nom_equipe'],
@@ -69,9 +70,9 @@ function getFormsData()
         'support_name' => $_POST['support_name'],
         'support_discord' => $_POST['support_discord'],
         'support_email' => $_POST['support_email'],
-        'have_logo' => $_POST['have_logo'],
-        'is_new' => $_POST['is_new'],
-        '@nb_old_player' => $_POST['nb_old_player'],
+        '@have_logo' => $_POST['have_logo'],
+        'motivation_text' => $_POST['motivation_text'],
+        'logo_url' => $target_file,
     );
 
     return $forms;
@@ -99,6 +100,13 @@ function parseForms($forms)
     return $parsed_forms;
 }
 
+/**
+ * Envoie un mail html
+ * @param string $to l'adresse mail du destinataire (peut être plusieurs adresses séparées par des virgules)
+ * @param string $subject
+ * @param string $message
+ * @return bool true si le mail a été envoyé, false sinon
+ */
 function sendMail($to, $subject, $message)
 {
     $from = "Oblivion Esports <noreply@oblivion-esport.fr>";
@@ -112,4 +120,22 @@ function sendMail($to, $subject, $message)
     );
 
     return mail($to, $subject, $message, $headers);
+}
+/**
+ * Sauvegarde les données dans la base de données
+ * @param array $forms le tableau des données du formulaire (nom_equipe, tag_equipe, ...) en string
+ */
+function saveToDB($forms)
+{
+    global $db;
+    try {
+        $query = "INSERT INTO TWC_INSCRIPTION (nom_equipe, tag_equipe, top_name, top_discord, top_email, jungle_name, jungle_discord, jungle_email, middle_name, middle_discord, middle_email, bot_name, bot_discord, bot_email, support_name, support_discord, support_email, logo_url, motivation_text, have_logo) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("ssssssssssssssssssss", $forms['nom_equipe'], $forms['tag_equipe'], $forms['top_name'], $forms['top_discord'], $forms['top_email'], $forms['jungle_name'], $forms['jungle_discord'], $forms['jungle_email'], $forms['middle_name'], $forms['middle_discord'], $forms['middle_email'], $forms['bot_name'], $forms['bot_discord'], $forms['bot_email'], $forms['support_name'], $forms['support_discord'], $forms['support_email'], $forms['logo_url'], $forms['motivation_text'], $forms['@have_logo']);
+        $stmt->execute();
+        $stmt->close();
+    } catch (Throwable $e) {
+        echo "Error during insertion : " . $e->getMessage() . PHP_EOL;
+        exit(1);
+    }
 }
