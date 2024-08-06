94bf2b08b56006fda0d87b199ca99c8c56924dee
UrbsKali <urbain.lantres@tutanota.com>
2024-08-07 00:53:06 +0200

    Tailwind + Auth UI

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -23,35 +23,78 @@
 	};
 </script>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget" aria-live="polite">
-		<p class="description">Sign in</p>
-		<form class="form-widget" on:submit|preventDefault={handleLogin}>
-			<div>
-				<label for="email">Email</label>
-				<input
-					id="email"
-					class="inputField"
-					type="email"
-					placeholder="Your email"
-					bind:value={email}
-				/>
+<section class=" bg-black bg-opacity-70">
+	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+		<a href="" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+			<img class="w-8 h-8 mr-2" src="/oblivion.png" alt="logo" />
+			Oblivion Esport
+		</a>
+		<div
+			class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+		>
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<h1
+					class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+				>
+					Panneau d'administration
+				</h1>
+				<form class="space-y-4 md:space-y-6" on:submit|preventDefault={handleLogin}>
+					<div>
+						<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Votre email</label
+						>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="kckitt@oblivion-esport.fr"
+							bind:value={email}
+						/>
+					</div>
+					<div>
+						<label
+							for="password"
+							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Votre mot de passe</label
+						>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="••••••••"
+							class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							bind:value={password}
+						/>
+					</div>
+					<div class="flex items-center justify-between">
+						<div class="flex items-start">
+							<div class="flex items-center h-5">
+								<input
+									id="remember"
+									aria-describedby="remember"
+									type="checkbox"
+									class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+								/>
+							</div>
+							<div class="ml-3 text-sm">
+								<label for="remember" class="text-gray-500 dark:text-gray-300"
+									>Se souvenir de moi</label
+								>
+							</div>
+						</div>
+					</div>
+					<button
+						type="submit"
+						disabled={loading}
+						class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+						>{loading ? 'Chargement ...' : 'Login'}</button
+					>
+				</form>
 			</div>
-			<div>
-				<label for="password">Password</label>
-				<input
-					id="password"
-					class="inputField"
-					type="password"
-					placeholder="********"
-					bind:value={password}
-				/>
-			</div>
-			<div>
-				<button type="submit" class="button block" aria-live="polite" disabled={loading}>
-					<span>{loading ? 'Loading' : 'Login'}</span>
-				</button>
-			</div>
-		</form>
+		</div>
 	</div>
-</div>
+</section>
+
+<style lang="postcss">
+</style>
