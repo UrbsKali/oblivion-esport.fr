a436ee6b97ff8ee321526500eb71888151ec884f
UrbsKali <urbain.lantres@tutanota.com>
2024-08-19 17:57:40 +0200

    Update +page.svelte

--- src/routes/admin/matchid/+page.svelte
+++ src/routes/admin/matchid/+page.svelte
@@ -98,7 +98,7 @@
 								>Tournament Code</label
 							>
 							<input
-								type="number"
+								type="text"
 								name="tournamentCode"
 								id="tournamentCode"
 								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
