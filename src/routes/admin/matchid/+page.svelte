fe95c59e304ef204a6e515363988e75c178d5933
UrbsKali <urbain.lantres@tutanota.com>
2024-08-30 23:44:25 +0200

    Update +page.svelte

--- src/routes/admin/matchid/+page.svelte
+++ src/routes/admin/matchid/+page.svelte
@@ -9,18 +9,18 @@
 
 	async function onSubmit() {
 		let resp = await fetch(
-			`https://oblivion-esport.fr/riot.php?endpoint=/riot/account/v1/accounts/by-riot-id/${username}/${tag}&secret=oF8Hz9pNp9fDQoaYarAe`
+			`https://oblivion-esport.fr/api/riot.php?endpoint=/riot/account/v1/accounts/by-riot-id/${username}/${tag}&secret=oF8Hz9pNp9fDQoaYarAe`
 		);
 		let data = await resp.json();
 		const puuid = data.puuid;
 		resp = await fetch(
-			`https://oblivion-esport.fr/riot.php?endpoint=/lol/match/v5/matches/by-puuid/${puuid}/ids&secret=oF8Hz9pNp9fDQoaYarAe`
+			`https://oblivion-esport.fr/api/riot.php?endpoint=/lol/match/v5/matches/by-puuid/${puuid}/ids&secret=oF8Hz9pNp9fDQoaYarAe`
 		);
 		data = await resp.json();
 		console.log(data);
 		const match_id = data[0];
 		resp = await fetch(
-			`https://oblivion-esport.fr/riot.php?endpoint=/lol/match/v5/matches/${match_id}&secret=oF8Hz9pNp9fDQoaYarAe`
+			`https://oblivion-esport.fr/api/riot.php?endpoint=/lol/match/v5/matches/${match_id}&secret=oF8Hz9pNp9fDQoaYarAe`
 		);
 		data = await resp.json();
 		console.log(data);
