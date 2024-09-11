6276b989915e53ae2bd9f52e9f586e93b6a96467
UrbsKali <urbain.lantres@tutanota.com>
2024-09-11 17:07:15 +0200

    Icon on stats

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -96,9 +96,9 @@
 
 <!-- Hero Search text input-->
 <div></div>
-<div class="flex w-screen justify-center items-center flex-col">
+<div class="flex flex-col items-center justify-center w-screen">
 	<h1 class="text-4xl font-semibold text-gray-900 dark:text-white">Rechercher un joueur</h1>
-	<div class="w-96 p-5">
+	<div class="p-5 w-96">
 		<div class="relative w-full">
 			<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 				<svg
@@ -118,14 +118,14 @@
 			<input
 				type="text"
 				id="simple-search"
-				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+				class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 				placeholder="KcKitt_#EUW"
 				required=""
 				bind:value={name}
 			/>
 			{#if isFocus || !(completion.length == 1 && name == completion[0])}
 				<div
-					class=" absolute z-10 mt-1 bg-gray-50 w-full border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+					class="absolute z-10 block w-full p-2 pl-10 mt-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 				>
 					{#each completion as c}
 						<button
@@ -149,22 +149,29 @@
 			<div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
 				<dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.kills}</dt>
+						<div class="w-10 h-10 bg-white" id="kill"></div>
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.kills}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Kills</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.deaths}</dt>
+						<div class="w-10 h-10 bg-white" id="death"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.deaths}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Deaths</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.assists}</dt>
+						<div class="w-10 h-10 bg-white" id="assist"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.assists}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Assists</dd>
 					</div>
 				</dl>
 				<br />
 				<dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+						<div class="w-10 h-10 bg-white" id="damage"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">
 							{prettyRound(stats.damageToChampions)}
 						</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">
@@ -172,28 +179,37 @@
 						</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.minions}</dt>
+						<div class="w-10 h-10 bg-white" id="minion"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.minions}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Minions Tués</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{prettyRound(stats.totalGold)}</dt>
+						<div class="w-10 h-10 bg-white" id="gold"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{prettyRound(stats.totalGold)}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Gold Amassés</dd>
 					</div>
 				</dl>
 				<br />
 				<dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+						<div class="w-10 h-10 bg-white" id="win"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">
 							{stats.win}
 						</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Victoires</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.lose}</dt>
+						<div class="w-10 h-10 bg-white" id="lose"></div>
+
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.lose}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Défaites</dd>
 					</div>
 					<div class="flex flex-col items-center justify-center">
-						<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stats.total}</dt>
+						<div class="w-10 h-10 bg-white" id="game"></div>
+						<dt class="mb-2 text-3xl font-extrabold md:text-4xl">{stats.total}</dt>
 						<dd class="font-light text-gray-500 dark:text-gray-400">Matchs joués</dd>
 					</div>
 				</dl>
@@ -203,4 +219,40 @@
 {/if}
 
 <style>
+	#lose {
+		mask-image: url('/stats/Armor_penetration_icon.png');
+		mask-size: 100% 100%;
+	}
+	#win {
+		mask-image: url('/stats/Win.png');
+		mask-size: 100% 100%;
+	}
+	#game {
+		mask-image: url('/stats/game.png');
+		mask-size: 100% 100%;
+	}
+	#gold {
+		mask-image: url('/stats/gold.png');
+		mask-size: 100% 100%;
+	}
+	#minion {
+		mask-image: url('/stats/Minion_icon.png');
+		mask-size: 100% 100%;
+	}
+	#damage {
+		mask-image: url('/stats/Damage.png');
+		mask-size: 100% 100%;
+	}
+	#assist {
+		mask-image: url('/stats/Assist.png');
+		mask-size: 100% 100%;
+	}
+	#death {
+		mask-image: url('/stats/Death.png');
+		mask-size: 100% 100%;
+	}
+	#kill {
+		mask-image: url('/stats/Kill.png');
+		mask-size: 100% 100%;
+	}
 </style>
