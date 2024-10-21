1ce1a3a739a904682434998935f210ff4150d025
UrbsKali <urbain.lantres@tutanota.com>
2024-10-21 02:16:31 +0100

    concept of the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,29 @@
 	import Cursor from '$lib/components/share/Cursor.svelte';
 	import Footer from '$lib/components/share/Footer.svelte';
 
+	let currentIndex = 0;
+	const images = [
+		{ src: 'https://placecats.com/350/200', alt: 'Image 1' },
+		{ src: 'https://placecats.com/350/200', alt: 'Image 2' },
+		{ src: 'https://placecats.com/350/200', alt: 'Image 3' }
+	];
+
+	const articles = [
+		{ title: 'Article 1', description: 'Description for article 1' },
+		{ title: 'Article 2', description: 'Description for article 2' },
+		{ title: 'Article 3', description: 'Description for article 3' }
+	];
+
 	onMount(() => {
 		const rl = document.querySelector('#rl');
-		const { width, height } = rl.getBoundingClientRect();
+		const twc = document.querySelector('#twc');
+
+		initRL3D(rl);
+		initTWC3D(twc);
+	});
+
+	function initTWC3D(renderElement) {
+		const { width, height } = renderElement.getBoundingClientRect();
 
 		const scene = new THREE.Scene();
 		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
@@ -18,7 +38,61 @@
 
 		const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 		renderer.setSize(width, height);
-		rl.appendChild(renderer.domElement);
+		renderElement.appendChild(renderer.domElement);
+
+		const gltfLoader = new GLTFLoader();
+		let car = null;
+		gltfLoader.load('/v2/3d/TWC.glb', (gltf) => {
+			scene.add(gltf.scene);
+			// scale it
+			gltf.scene.scale.set(1, 1, 1);
+
+			// rotate it
+			gltf.scene.children[0].rotation.x = -Math.PI / 8;
+			gltf.scene.children[0].rotation.x = -Math.PI / 8;
+
+			// move down
+			gltf.scene.position.y = -0.25;
+
+			// cast shadow
+			gltf.scene.children[0].castShadow = true;
+
+			car = gltf.scene;
+		});
+
+		// add lights
+		const light = new THREE.AmbientLight(0xffffff, 1);
+		light.position.set(0, 0, 10);
+		scene.add(light);
+		const light2 = new THREE.DirectionalLight(0xffffff, 5);
+		light2.position.set(0, 10, 1);
+		scene.add(light2);
+		const light3 = new THREE.DirectionalLight(0xffffff, 5);
+		light3.position.set(0, -10, 1);
+		scene.add(light3);
+		const light4 = new THREE.DirectionalLight(0xffffff, 5);
+		light4.position.set(10, 0, 1);
+
+		const animate = () => {
+			if (car) {
+				car.rotation.y += 0.001;
+			}
+			renderer.render(scene, camera);
+			requestAnimationFrame(animate);
+		};
+		requestAnimationFrame(animate);
+	}
+
+	function initRL3D(renderElement) {
+		const { width, height } = renderElement.getBoundingClientRect();
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		camera.position.z = 5;
+
+		const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+		renderer.setSize(width, height);
+		renderElement.appendChild(renderer.domElement);
 
 		const gltfLoader = new GLTFLoader();
 		let car = null;
@@ -40,7 +114,7 @@
 		});
 
 		// add lights
-		const light = new THREE.AmbientLight(0xffffff, 10);
+		const light = new THREE.AmbientLight(0xffffff, 1);
 		light.position.set(0, 0, 10);
 		scene.add(light);
 		const light2 = new THREE.DirectionalLight(0xffffff, 5);
@@ -60,7 +134,7 @@
 			requestAnimationFrame(animate);
 		};
 		requestAnimationFrame(animate);
-	});
+	}
 </script>
 
 <div class="border-b border-gray-700 landing enable-cursor">
@@ -81,15 +155,19 @@
 		></video>
 	</div>
 </div>
-<div class="flex flex-row justify-center w-full gap-5 border-b border-gray-700">
-	<div class="w-6/12 aspect-square min-w-96" id="rl"></div>
-	<!--Add text floating next to the crown-->
-	<div>
-		<div class="flex flex-col items-center justify-center h-96">
+<div class="w-full border-b border-gray-700">
+	<div class="flex flex-col items-center justify-center w-full p-5">
+		<h1 class="text-4xl font-bold text-white">Nos Tournois</h1>
+	</div>
+
+	<!-- ELEON WOLRD CUP -->
+	<div class="flex flex-col justify-center gap-5 sm:flex-row">
+		<div class="w-6/12 aspect-square min-w-96" id="rl"></div>
+		<div class="flex flex-col items-center justify-center">
 			<div class="flex flex-col items-center justify-center w-9/12 h-full">
 				<div class="text-center">
 					<h1 class="text-4xl font-bold text-gray-300 w-max">ELEON WORLD CUP</h1>
-					<p class="text-lg text-gray-300">300 € de cashprize, en seriez-vous digne ?</p>
+					<p class="text-lg text-gray-300">300 € de cashprize, en êtes-vous digne ?</p>
 					<div>
 						<button class="px-4 py-2 mt-5 text-white border rounded-md border-primary-500"
 							>En savoir +</button
@@ -100,14 +178,79 @@
 			</div>
 		</div>
 	</div>
+	<!-- TWC 5 -->
+	<div class="flex flex-col justify-center gap-5 sm:flex-row">
+		<div class="flex flex-col items-center justify-center">
+			<div class="flex flex-col items-center justify-center w-9/12 h-full">
+				<div class="text-center">
+					<h1 class="text-4xl font-bold text-gray-300 w-max">TRANKIL WOLD CUP</h1>
+					<p class="text-lg text-gray-300">500 € de cashprize, Nabot viens m'aider !</p>
+					<div>
+						<button class="px-4 py-2 mt-5 text-white border rounded-md border-primary-500"
+							>En savoir +</button
+						>
+						<button class="px-4 py-2 mt-5 text-white rounded-md bg-primary-500">Inscription</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-6/12 aspect-square min-w-96" id="twc"></div>
+	</div>
 </div>
 <div class="border-b border-gray-700 lol">
-	<div class="h-screen"></div>
+	<div class="flex flex-col items-center justify-center w-full p-5">
+		<h1 class="text-4xl font-bold text-white">Nos Actus</h1>
+	</div>
+	<div class="h-screen">
+		<div class="flex justify-center h-full">
+			<!-- Carousel -->
+			<div class="w-1/4 h-full overflow-hidden">
+				<div class="flex flex-col gap-5 carousel">
+					{#each images as image, index}
+						<div class="carousel-item {index === currentIndex ? 'active' : ''}">
+							<img src={image.src} alt={image.alt} class="object-cover rounded-lg h-52" />
+						</div>
+					{/each}
+				</div>
+			</div>
+
+			<!-- Article Details -->
+			<div class="p-5 pt-10 text-white mt-52">
+				<h2 class="text-3xl font-bold">{articles[currentIndex].title}</h2>
+				<p class="mt-4 text-lg">{articles[currentIndex].description}</p>
+			</div>
+		</div>
+	</div>
 </div>
 <Cursor />
 <Footer />
 
 <style>
+	.carousel {
+		display: flex;
+		transition: transform 0.5s ease-in-out;
+	}
+	.carousel::after {
+		content: '';
+		width: 100%;
+		height: 41.5rem;
+		top: -42.75rem;
+		position: relative;
+		background: linear-gradient(
+			rgba(17, 24, 39, 1) 0%,
+			rgba(0, 0, 0, 0) 50%,
+			rgba(17, 24, 39, 1) 100%
+		);
+		z-index: 1;
+	}
+	.carousel-item {
+		min-width: 100%;
+		transition: opacity 0.5s ease-in-out;
+		opacity: 1;
+	}
+	.carousel-item.active {
+		opacity: 1;
+	}
 	.landing {
 		filter: brightness(0.8);
 		height: 100vh;
