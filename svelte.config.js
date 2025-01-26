4ee1a10578d677df0585c9867c3293a8d3318c17
UrbsKali <urbain.lantres@tutanota.com>
2025-01-26 22:46:20 +0100

    Config Coolify

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
-let extra_entries = [];
+// let extra_entries = [];
 
-// Add all the pages in the /user/teams/ directory
-for (let i = 1; i <= 100; i++) {
-	extra_entries.push(`/user/teams/${i}`);
-	extra_entries.push(`/admin/teams/${i}`);
-	extra_entries.push(`/admin/register/${i}`);
-}
+// // Add all the pages in the /user/teams/ directory
+// for (let i = 1; i <= 100; i++) {
+// 	extra_entries.push(`/user/teams/${i}`);
+// 	extra_entries.push(`/admin/teams/${i}`);
+// 	extra_entries.push(`/admin/register/${i}`);
+// }
 
 
 
@@ -19,15 +19,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		prerender: {
-			entries: ['*', '/admin/tournaments/eleon-world-cup', '/admin/tournaments/trankil-world-cup5', ...extra_entries],
+			entries: ['*', '/admin/tournaments/eleon-world-cup', '/admin/tournaments/trankil-world-cup5'],
 		},
 	},
 
