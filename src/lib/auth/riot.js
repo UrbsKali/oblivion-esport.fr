d81d413c40fbe7476814d30426793f9226cf5140
UrbsKali <urbain.lantres@tutanota.com>
2025-03-12 21:22:45 +0100

    Update riot.js

--- src/lib/auth/riot.js
+++ src/lib/auth/riot.js
@@ -6,7 +6,7 @@ export const ClientID = "3ba9b93b-8c9e-4e5d-a164-95c8fadf2a99";
 
 export function createOauthUrl(redirect_to = "https://oblivion-esport.fr/login/riot") {
     // https://auth.riotgames.com/authorize?client_id={your-client-id}&redirect_uri={your-redirect-uri}&response_type=code&scope=openid+offline_access.
-    return `https://auth.riotgames.com/authorize?client_id=${ClientID}&redirect_uri=${redirect_to}&response_type=code&scope=openid`;
+    return `https://auth.riotgames.com/authorize?client_id=${ClientID}&redirect_uri=${redirect_to}&response_type=code&scope=openid+offline_access`;
 }
 
 export async function getAccessToken(code) {
